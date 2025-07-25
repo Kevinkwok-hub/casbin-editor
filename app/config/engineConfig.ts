ba4a427be10ea55c9d54e2471aea016516c6e79e
kevin kwok <13146699620ke@gmail.com>
2025-07-25 22:11:51 +0800

    feat: show python engine and CLI versions in engine dropdown widget

--- app/config/engineConfig.ts
+++ app/config/engineConfig.ts
@@ -21,6 +21,7 @@ export const ENGINES = {
       return createCasbinEngine('node');
     },
   },
+ 
   java: {
     id: 'java',
     name: 'jCasbin (Java)',
@@ -51,6 +52,16 @@ export const ENGINES = {
       return new RemoteCasbinEngine('rust');
     },
   },
+   python: {  
+  id: 'python',  
+  name: 'PyCasbin (Python)',  
+  githubRepo: 'casbin/casbin-python-cli',  
+  isRemote: true,  
+  order: 5,  
+  createEngine: () => {  
+    return new RemoteCasbinEngine('python');  
+  },  
+},
 } as const;
 
 export type EngineType = keyof typeof ENGINES;
